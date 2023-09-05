60379724029324c45e6fc70c905b9c3b4c309cc5
julieyoung6 <104098435+julieyoung6@users.noreply.github.com>
2023-09-05 17:13:33 -0400

    commenting and reorganizing functions - final edits

--- simulator.py
+++ simulator.py
@@ -1,87 +1,8 @@
-def find_reaction_index(species, reactants):
-    for i, reactant_list in enumerate(reactants):
-        if species in reactant_list:
-            return i
-    
-def find_reversible_reaction(reactants, products):
-    # finds the species that are involved in a reversible reaction
-
-    collapsed_reactions = []
-    reversible_reactions = []
-
-    # create a string for each reaction
-    for i, reactant in enumerate(reactants):
-        for j in range(len(products[i])):
-            reaction_str = reactant[0] + products[i][j]
-            collapsed_reactions.append(reaction_str)
-
-    # find reversible reactions
-    rr_species = []
-    for r in collapsed_reactions:
-        if collapsed_reactions.count(r[::-1]) > 0:
-            collapsed_reactions[collapsed_reactions.index(r[::-1])] = "-."
-            print(r)
-            if r[0] not in rr_species and r[1] not in rr_species:
-                reversible_reactions.append([[r[0]],[r[1]]])
-                rr_species.append(r[0])
-                rr_species.append(r[1])
-            elif r[0] in rr_species:
-                for rr in reversible_reactions:
-                    if [r[0]] in rr:
-                        rr.append([r[1]])
-                        rr_species.append(r[1])
-            elif r[1] in rr_species:
-                for rr in reversible_reactions:
-                    if [r[1]] in rr:
-                        rr.append([r[0]])
-                        rr_species.append(r[0])
-
-    return reversible_reactions, rr_species
-
-def external_species_extrapolation(starting_species, rr_sources, rr_species, species, reactants, reactant_coeffs, products, product_coeffs, reaction_rate_constants, final_species_amount, current_species_amount):
-    # extrapolates final yield for external species of a reversible reaction
-
-    cycle_starting_amount = current_species_amount[species.index(starting_species[0])]
-    # find external species
-    external_species = []
-    for i, reactant in enumerate(reactants):
-        if reactant in rr_sources and products[i] not in rr_sources:
-            external_species.append(products[i])
-    ten_step_vals = []
-    ten_step_source_out_of_cycle = 0
-    twenty_step_vals = []
-    twenty_step_source_out_of_cycle = 0
-
-    # run cycle for 20 steps
-    source = starting_species
-    for i in range(20):
-        next_source = []
-        validated_sources = [x for x in source if [x] in rr_sources]
-       
-        final_species_amount, current_species_amount, next_sources = cycle(validated_sources, species, reactants, reactant_coeffs, products, product_coeffs, reaction_rate_constants, final_species_amount, current_species_amount)
-        for ns in next_sources:
-            next_source.append(ns)
-        next_source = list(set(next_source))
-        source = next_source
- 
-        # collect data at 10 steps
-        if i == 9:
-            for es in external_species:
-                ten_step_vals.append(final_species_amount[species.index(es[0])])
-                ten_step_source_out_of_cycle += final_species_amount[species.index(es[0])]
-    # collect data after 20 steps
-    for es in external_species:
-        twenty_step_vals.append(final_species_amount[species.index(es[0])])
-        twenty_step_source_out_of_cycle += final_species_amount[species.index(es[0])]
-
-    # extrapolate each external species value if 100% of the source left the cycle
-    for i, es in enumerate(external_species):
-        print(es, twenty_step_vals[i], ten_step_vals[i], twenty_step_source_out_of_cycle, ten_step_source_out_of_cycle)
-        final_species_amount[species.index(es[0])] = ((twenty_step_vals[i] - ten_step_vals[i])/(twenty_step_source_out_of_cycle - ten_step_source_out_of_cycle) * (1-twenty_step_source_out_of_cycle) + twenty_step_vals[i])  * cycle_starting_amount 
-
-    return external_species, final_species_amount
+import math
 
 def cycle(source_species, species, reactants, reactant_coeffs, products, product_coeffs, reaction_rate_constants, final_species_amount, current_species_amount):
+    # modified version of simple reaction 
+    
     next_sources = []
 
     # solve each source branch
@@ -116,9 +37,89 @@ def cycle(source_species, species, reactants, reactant_coeffs, products, product
 
     return final_species_amount, current_species_amount, next_sources
 
+def external_species_extrapolation(starting_species, rr_sources, species, reactants, reactant_coeffs, products, product_coeffs, reaction_rate_constants, final_species_amount, current_species_amount):
+    # extrapolates final yield for external species of a reversible reaction
+
+    cycle_starting_amount = current_species_amount[species.index(starting_species[0])]
+
+    # find external species
+    external_species = []
+    for i, reactant in enumerate(reactants):
+        if reactant in rr_sources and products[i] not in rr_sources:
+            external_species.append(products[i])
+
+    ten_step_vals = []
+    ten_step_source_out_of_cycle = 0
+    twenty_step_vals = []
+    twenty_step_source_out_of_cycle = 0
+
+    # run cycle for 20 steps
+    source = starting_species
+    for i in range(20):
+        next_source = []
+
+        # only allows for species in the reversible reaction to be inputed as source species
+        validated_sources = [x for x in source if [x] in rr_sources]
+       
+        final_species_amount, current_species_amount, next_sources = cycle(validated_sources, species, reactants, reactant_coeffs, products, product_coeffs, reaction_rate_constants, final_species_amount, current_species_amount)
+        for ns in next_sources:
+            next_source.append(ns)
+        next_source = list(set(next_source))
+        source = next_source
+ 
+        # collect data at 10 steps
+        if i == 9:
+            for es in external_species:
+                ten_step_vals.append(final_species_amount[species.index(es[0])])
+                ten_step_source_out_of_cycle += final_species_amount[species.index(es[0])]
+    # collect data after 20 steps
+    for es in external_species:
+        twenty_step_vals.append(final_species_amount[species.index(es[0])])
+        twenty_step_source_out_of_cycle += final_species_amount[species.index(es[0])]
+
+    # extrapolate each external species value under the assumption that 100% of the source left the cycle
+    for i, es in enumerate(external_species):
+        final_species_amount[species.index(es[0])] = ((twenty_step_vals[i] - ten_step_vals[i])/(twenty_step_source_out_of_cycle - ten_step_source_out_of_cycle) * (1-twenty_step_source_out_of_cycle) + twenty_step_vals[i])  * cycle_starting_amount 
+
+    return external_species, final_species_amount
+
+def find_reversible_reaction(reactants, products):
+    # finds the species that are involved in a reversible reaction
+
+    collapsed_reactions = []
+    reversible_reactions = []
+
+    # create a string for each reaction
+    for i, reactant in enumerate(reactants):
+        for j in range(len(products[i])):
+            reaction_str = reactant[0] + products[i][j]
+            collapsed_reactions.append(reaction_str)
+
+    # find reversible reactions
+    rr_species = []
+    for r in collapsed_reactions:
+        if collapsed_reactions.count(r[::-1]) > 0:
+            collapsed_reactions[collapsed_reactions.index(r[::-1])] = "-."
+            if r[0] not in rr_species and r[1] not in rr_species:
+                reversible_reactions.append([[r[0]],[r[1]]])
+                rr_species.append(r[0])
+                rr_species.append(r[1])
+            elif r[0] in rr_species:
+                for rr in reversible_reactions:
+                    if [r[0]] in rr:
+                        rr.append([r[1]])
+                        rr_species.append(r[1])
+            elif r[1] in rr_species:
+                for rr in reversible_reactions:
+                    if [r[1]] in rr:
+                        rr.append([r[0]])
+                        rr_species.append(r[0])
+
+    return reversible_reactions, rr_species
 
 def simple_reaction(source_species, source_amount, species, reactants, reactant_coeffs, products, product_coeffs, reaction_rate_constants, final_species_amount, current_species_amount):
-    reaction_prod = []
+    # takes a given species and solves the reactions involving that species as its reactant 
+    
     next_sources = []
     branches_i = []
     sum_rate_constants = 0
@@ -141,7 +142,6 @@ def simple_reaction(source_species, source_amount, species, reactants, reactant_
         
         for p in range(len(products[b])):
             p_amount = (reaction_rate_constants[b]/sum_rate_constants) * source_amount * product_coeffs[b][p]
-            reaction_prod.append([products[b][p], p_amount])
 
              # assign values to final and current amounts
             final_species_amount[species.index(products[b][p])] += p_amount
@@ -151,6 +151,11 @@ def simple_reaction(source_species, source_amount, species, reactants, reactant_
     return final_species_amount, current_species_amount, next_sources
 
 def sim(species, reactants, reactant_coeffs, products, product_coeffs, reaction_rate_constants):
+    # main function
+    # --- input example: ["A", "B", "C"], [["A"], ["A"]], [[1], [1]], [["B"], ["C"]], [[1], [1]], [1, 1]
+    # --- output: species list and list of corresponding cumulative amount generated
+    # --- output example: (['A', 'B', 'C'], [1, 0.5, 0.5])
+
     # create empty lists with same len as species
     final_species_amount = [1] * len(species)  # cumulative amount
     current_species_amount = [0] * len(species)
@@ -180,7 +185,8 @@ def sim(species, reactants, reactant_coeffs, products, product_coeffs, reaction_
                 for i, rr in enumerate(reversible_reactions):
                     if [s] in rr:
                         rr_index = i
-                external_species, final_species_amount = external_species_extrapolation([s], reversible_reactions[rr_index], rr_species, species, reactants, reactant_coeffs, products, product_coeffs, reaction_rate_constants, final_species_amount, current_species_amount)
+
+                external_species, final_species_amount = external_species_extrapolation([s], reversible_reactions[rr_index], species, reactants, reactant_coeffs, products, product_coeffs, reaction_rate_constants, final_species_amount, current_species_amount)
                 current_species_amount = final_species_amount.copy() 
                 for rr_source in reversible_reactions[rr_index]:
                     current_species_amount[species.index(rr_source[0])] = 0
@@ -197,36 +203,47 @@ def sim(species, reactants, reactant_coeffs, products, product_coeffs, reaction_
         next_source = list(set(next_source))
         source = next_source
 
-    # prints each species and final value 
+    # prints each species and final value (breaks up final lists for easy viewing)
     for i, s in enumerate(species):
         print(s, final_species_amount[i])
 
     return species, final_species_amount
 
+def test_multi_reactant_mech(k1_input):
+    # hard coded test multi reactant mech
 
-# print(sim(["A", "B", "C","D", "E", "F"],[["A"],["B"],["B"],["C"],["C"], ["E"]],[[1],[1],[1],[1],[1], [1]], [["B"],["C"],["D"],["B"],["E"], ["F"]],[[1],[1],[1],[1],[1],[1]], [1,1,1,1,1,1]))
-# print(sim(["A", "B", "C","D", "E", "F"],[["A"],["B"],["C"],["C"], ["E"],["B"]],[[1],[1],[1],[1],[1], [1]], [["B"],["D"],["B"],["E"], ["F"],["C"]],[[1],[1],[1],[1],[1],[1]], [1,1,20,1,1,20]))
-
-
-# # Cycle w/ more than 2 species B<-->C<-->E
-# print(sim(["A", "B", "C","D", "E", "F"],[["A"],["B"],["B"],["C"],["C"], ["E"], ["E"]],[[1],[1],[1],[1],[1], [1],[1]], [["B"],["C"],["D"],["B"],["E"], ["C"], ["F"]],[[1],[1],[1],[1],[1],[1],[1]], [1,200,1,200,1,1,1]))
-
-# # Disconnected Cycle Test B<-->C and E<-->F
-# print(sim(["A", "B", "C","D", "E", "F", "G"],[["A"],["B"],["B"],["C"],["C"], ["E"], ["F"], ["F"]],[[1],[1],[1],[1],[1], [1],[1],[1]], [["B"],["C"],["D"],["B"],["E"], ["F"], ["E"], ["G"]],[[1],[1],[1],[1],[1],[1],[1],[1]], [1,1,1,1,1,1,1,1]))
-
-# Multiple Reactants Step 1
-# print(sim(["A", "B", "C", "D"], [["A","B"],["A"]], [[1,1],[1]], [["D"],["C"]], [[1],[1]],[1,1]))
+    species = ['A', 'B', 'C', 'D', 'F', 'G', 'H']
+    A_conc = 1
+    final_species_amount = [A_conc, A_conc/2, A_conc/2, 0, 0, A_conc/2, 0]
+    k1 = k1_input
+    k2 = 1
+    k3 = 1
 
 
-# Test Mini Dag Reaction
-# sim(["A", "B", "C","D"], [["A"],["A"],["A"],["B"],["C"]], [[1],[1],[1],[1],[1]], [["B"],["C"],["D"],["C"],["D"]],[[1],[1],[1],[1],[1]],[1,1,1,1,1] )
-# create_f0am_file(["A", "B", "C","D"], [["A"],["A"],["A"],["B"],["C"]], [[1],[1],[1],[1],[1]], [["B"],["C"],["D"],["C"],["D"]],[[1],[1],[1],[1],[1]],[1,1,1,1,1], 'mini_test_dag_reaction')
+    F_D = (A_conc * k2)/(2 * k3) * (1 - math.exp(-(1*k1)/k3)) * 13.2
+    #F
+    final_species_amount[4] = (final_species_amount[2] * F_D) / (A_conc + F_D) 
+    #D
+    final_species_amount[3] = final_species_amount[4] * (1/F_D)
+    #H
+    final_species_amount[6] = A_conc - (final_species_amount[4]*2) - final_species_amount[3]
 
+    # prints F, D, (k_1, F/D)
+    print(final_species_amount[4], final_species_amount[3], "(",  k1, ",",final_species_amount[4]/final_species_amount[3],  ")")
+
+
+# test_rates = [.01,.1,.5,1,2,5,20,50,100]
+# for rate in test_rates:
+#     test_multi_reactant_mech(rate)
+
+
+
+# use data from CSV to run sim()
 import pandas as pd
 
-# df = pd.read_csv('tree_reactions.csv', header=None)
+df = pd.read_csv('tree_reactions.csv', header=None)
 # df = pd.read_csv('dag_reactions.csv', header=None)
-df = pd.read_csv('cyclic_graph_reactions.csv', header=None)
+# df = pd.read_csv('cyclic_graph_reactions.csv', header=None)
 df.columns = ["reactants", "products"]
 
 reactants = []
@@ -247,4 +264,4 @@ for i, r in enumerate(df["reactants"]):
 species.sort()
 
 sim(species, reactants, coeffs, products, coeffs.copy(), rrconstants)
-# create_f0am_file(species, reactants, coeffs, products, coeffs.copy(), rrconstants, 'cyclic_reactions')
+# use print() around the sim(...) to print the returned values (list of species, list of corresponding cumulative amounts)
